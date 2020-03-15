49a2608d16f432579dd0512dfe07dac6bd292ef2
sunshine <fejavu@foxmail.com>
2020-03-15 12:36:12 +0800

    leetcode 283.moveZero two more solutions added.

--- 283.移动零.js
+++ 283.移动零.js
@@ -10,6 +10,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+  // moveZeroesSolu_one(nums);
+  // moveZeroesSolu_two(nums);
+  moveZeroesSolu_three(nums)
+
+};
+
+let moveZeroesSolu_one = function(nums) {
   let mark = 0;
 
   for(let i=0; i<nums.length; i++) {
@@ -23,5 +30,35 @@ var moveZeroes = function(nums) {
     nums[mark] = 0;
     mark++;
   }
-};
+}
+
+let moveZeroesSolu_two = function(nums) {
+  let mark = 0;
+
+  for(let i=0; i<nums.length; i++) {
+    if(nums[i] !==0) {
+      let tempItem = nums[mark];
+      nums[mark] = nums[i];
+      nums[i] = tempItem;
+      mark++;
+    }
+  }
+}
+
+let moveZeroesSolu_three = function(nums) {
+  let nonZeroArr = [];
+
+  nums.forEach(element => {
+    if(element !== 0)
+      nonZeroArr.push(element);
+  });
+
+  for(let i=0; i<nonZeroArr.length; i++) {
+    nums[i] = nonZeroArr[i];
+  }
+
+  for(let j=nonZeroArr.length; j<nums.length; j++) {
+    nums[j] = 0;
+  }
+}
 // @lc code=end
