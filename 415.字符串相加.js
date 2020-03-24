d77a773003f8dd2ef75602fdf831c4f6494c6f85
sunshine <fejavu@foxmail.com>
2020-03-24 23:51:27 +0800

    leetcode 415.addstring

---
+++ 415.字符串相加.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=415 lang=javascript
+ *
+ * [415] 字符串相加
+ */
+
+// @lc code=start
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+  return addStringsSolu_one(num1, num2);
+};
+
+let addStringsSolu_one = function(num1, num2) {
+  let res = '';
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+  let carry = 0;
+
+  
+}
+
+// @lc code=end
+
