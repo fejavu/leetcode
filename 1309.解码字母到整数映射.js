e80226224913e1f46ee8e8afe411515eac194928
sunshine <fejavu@foxmail.com>
2020-04-14 23:53:59 +0800

    leetCode 43.multiply and 1309

---
+++ 1309.解码字母到整数映射.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1309 lang=javascript
+ *
+ * [1309] 解码字母到整数映射
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var freqAlphabets = function(s) {
+
+};
+// @lc code=end
+
--- 43.字符串相乘.js
+++ 43.字符串相乘.js
@@ -11,31 +11,30 @@
  * @return {string}
  */
 var multiply = function(num1, num2) {
-  let len1 = num1.length;
-  let len2 = num2.length;
-  let pos = Array(len1+len2);
+  if(num1==='0' || num2==='0') return '0';
+  num1 = num1+'';
+  num2 = num2+'';
 
-  for(let i=len1-1; i>=0; i--) {
-    for(let j=len2-1; j>=0; j--) {
-      let mul = (num1.charAt(i)-'0')*(num2.charAt(j) -'0');
-      let pos1 = i+j, pos2 = i+j+1;
-      let sum = mul + pos[pos2];
+  let len1 = num1.length, len2 = num2.length;
+  let cache = new Array(len1+len2-1).fill(0);
+  let res='', temp = 0; 
 
-      pos[pos1] += sum/10;
-      pos[pos2] = sum%10;
+  for(let i=0; i<len2; i++) {
+    for(let j=0;j<len1; j++) {
+      cache[i+j] += (num2.charAt(i)-'0')*(num1.charAt(j)-'0');
     }
   }
+  let count = cache.length;
+  
+  while(count--) {
+    temp += cache[count];
+    res = temp%10 + res;
+    temp = Math.floor(temp/10);
+    // temp = temp / 10 | 0;
+  }
 
-  let str = '';
-  pos.forEach(item => {
-    if(!(str.length==0 && item==0)) {
-      str += item;
-    }
-  });
-
-  return str.length==0 ? '0' : str;
+  return temp>0 ? (temp+res) : res;
 };
-
 
 // @lc code=end
 
