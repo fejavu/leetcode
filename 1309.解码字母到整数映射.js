bf795af4491a83c4161e92c3c634f9dc97c4920a
sunshine <fejavu@foxmail.com>
2020-04-17 00:18:15 +0800

    leetCode 459. repeated Substring Pattern

--- 1309.解码字母到整数映射.js
+++ 1309.解码字母到整数映射.js
@@ -10,7 +10,17 @@
  * @return {string}
  */
 var freqAlphabets = function(s) {
+  let res = '';
 
+  for(let i=0; i<s.length; i++) {
+    if(s[i+2]==='#') {
+      res = res + String.fromCharCode((s[i]+s[i+1] - '0') + 96);
+      i = i + 2;
+    } else {
+      res = res + String.fromCharCode(s[i]-'0' + 96);
+    }
+  }
+  return res;
 };
 // @lc code=end
 
